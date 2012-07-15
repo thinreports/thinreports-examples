51831af081bf158834de05d924a12ce1ce7741bc
Katsuya Hidaka <work.hidaka@gmail.com>
2012-07-16 01:18:23 +0900

    added eudc example

---
+++ eudc/eudc.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+require 'rubygems'
+require 'thinreports'
+
+ThinReports.configure do
+  generator.pdf.eudc_fonts = 'eudc.ttf'
+end
+
+ThinReports::Report.generate_file('eudc.pdf', :layout => 'eudc.tlf') do
+  start_new_page
+  page.item(:eudc).value("日本で生まれ世界が育てた言語\nuby") # "□" は外字
+end
+
