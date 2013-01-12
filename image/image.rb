d99591d9c9fad9e57c3e29c94d0108813d461945
Katsuya Hidaka <work.hidaka@gmail.com>
2013-01-12 23:56:32 +0900

    add example to embed the image dynamically

---
+++ image/image.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+require 'thinreports'
+require 'open-uri'
+
+report = ThinReports::Report.new :layout => 'image.tlf'
+report.start_new_page do |page|
+  page.item(:local_image).src('file/rails.png')
+  page.item(:remote_image).src(open('http://rubyonrails.org/images/rails.png'))
+end
+
+report.generate_file 'image.pdf'
