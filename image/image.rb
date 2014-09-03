eae8606c154f10dba178d5dca097fe646d513e50
maeda-m <hogehogefoozoobar@gmail.com>
2014-09-03 21:24:46 +0900

    Updated Example: image

--- image/image.rb
+++ image/image.rb
@@ -3,10 +3,10 @@
 require 'thinreports'
 require 'open-uri'
 
-report = ThinReports::Report.new :layout => 'image.tlf'
+report = ThinReports::Report.new layout: 'image.tlf'
 report.start_new_page do |page|
   page.item(:local_image).src('file/rails.png')
   page.item(:remote_image).src(open('http://rubyonrails.org/images/rails.png'))
 end
 
-report.generate_file 'image.pdf'
+report.generate filename: 'image.pdf'
