450da226ec4ce0e2e2955d7964c086fb05fc359d
maeda-m <hogehogefoozoobar@gmail.com>
2014-09-03 21:18:11 +0900

    Updated Example: estimate

---
+++ estimate-ja/estimate.rb
@@ -0,0 +1,100 @@
+# coding: utf-8
+
+require 'thinreports'
+
+# Prepare sample data.
+data = []
+
+# Sample Data 1
+d1 = {:no          => 1234,
+      :created_d   => Time.now,
+      :customer    => '㈱テストコム',
+      :title       => 'PC及び関連機器ご購入について',
+      :price       => 1000000,
+      :tax         => 50000,
+      :total_price => 1050000,
+      :details     => []}
+d1[:details] << {:no         => 1,
+                 :title      => 'ノートパソコン',
+                 :amount     => 4,
+                 :unit_price => 200000,
+                 :price      => 800000}
+d1[:details] << {:no         => 2,
+                 :title      => 'プリンタ',
+                 :amount     => 2,
+                 :unit_price => 100000,
+                 :price      => 200000,
+                 :note       => 'トナー×2本含む'}
+ 
+# Sample Data 2
+d2 = {:no          => 3456,
+      :created_d   => '2011年1月1日',
+      :customer    => '㈱○○○○',
+      :title       => '□□□□□□□□□',
+      :price       => 100000,
+      :tax         => 5000,
+      :total_price => 105000,
+      :note        => '△' * 40,
+      :details     => []}
+20.times do |t|
+  d2[:details] << {:no         => t + 1,
+                   :title      => '×××××××××',
+                   :amount     => 1,
+                   :unit_price => 5000,
+                   :price      => 5000}
+end
+
+data << d1 << d2
+
+# Generate reports.
+report = ThinReports::Report.create do |r|
+  # Setting the layout for 'estimate.tlf'
+  r.use_layout 'estimate.tlf' do |config|
+    # Setting the :details list.
+    config.list(:details) do
+      use_stores :price       => 0,
+                 :total_price => 0
+
+      # Dispatch at list-page-footer insertion.
+      events.on :page_footer_insert do |e|
+        # Set subtotal price.
+        e.section.item(:price).value(e.store.price)
+        # Initialize subtotal price to 0.
+        e.store.price = 0
+      end
+
+      # Dispatch at list-footer insertion.
+      events.on :footer_insert do |e|
+        # Set total price.
+        e.section.item(:price).value(e.store.total_price)
+      end
+    end
+  end
+
+  data.each do |header|
+    r.start_new_page
+
+    # Set header datas.
+    r.page.values(:no          => header[:no],
+                  :created_d   => header[:created_d],
+                  :customer    => header[:customer],
+                  :title       => header[:title],
+                  :price       => header[:price],
+                  :tax         => header[:tax],
+                  :total_price => header[:total_price],
+                  :note        => header[:note])
+
+    header[:details].each do |detail|
+      # Add an row of list.
+      r.page.list(:details).add_row(detail)
+
+      # Calculate the price.
+      r.page.list(:details) do |list|
+        list.store.price       += detail[:price]
+        list.store.total_price += detail[:price]
+      end
+    end
+  end
+end
+
+report.generate filename: 'estimate.pdf'
--- estimate/estimate.rb
+++ estimate/estimate.rb
@@ -1,101 +1,121 @@
 # coding: utf-8
 
-require 'rubygems'
 require 'thinreports'
 
 # Prepare sample data.
-estimates = []
+data = []
+
+my_info = {
+  my_name: 'Matsukei Co., Ltd.',
+  my_address: '735-211, Nogifukutomicho, Matsue-shi, Shimane, Japan',
+  my_post_code: '690-0046',
+  my_tel_number: '+81-854-32-1616',
+  my_fax_number: '+81-852-32-1629'
+}
 
 # Sample Data 1
-d1 = {:no          => 1234,
-      :created_d   => Time.now,
-      :customer    => '㈱テストコム',
-      :title       => 'PC及び関連機器ご購入について',
-      :price       => 1000000,
-      :tax         => 50000,
-      :total_price => 1050000,
-      :details     => []}
-d1[:details] << {:no         => 1,
-                 :title      => 'ノートパソコン',
-                 :amount     => 4,
-                 :unit_price => 200000,
-                 :price      => 800000}
-d1[:details] << {:no         => 2,
-                 :title      => 'プリンタ',
-                 :amount     => 2,
-                 :unit_price => 100000,
-                 :price      => 200000,
-                 :note       => 'トナー×2本含む'}
+d1 = {
+  no: 1234,
+  issued_date: Time.now,
+  customer_name: 'Sample1 Co., Ltd.',
+  customer_address: '1234, Sample1cho, Sample1-shi, Shimane, Japan',
+  customer_post_code: '123-4567',
+  details: [],
+  notes: 'Estimate exsample1!'
+}
+
+d1.merge!(my_info)
+
+d1[:details] << {
+  no: 1,
+  name: 'iPon6',
+  rate: 199,
+  qty: 1,
+  amount: 199
+}
+d1[:details] << {
+  no: 2,
+  name: 'PearBook Pro 13-inch: 2.9GHz',
+  rate: 1499,
+  qty: 2,
+  amount: 2998
+}
 
 # Sample Data 2
-d2 = {:no          => 3456,
-      :created_d   => '2011年1月1日',
-      :customer    => '㈱○○○○',
-      :title       => '□□□□□□□□□',
-      :price       => 100000,
-      :tax         => 5000,
-      :total_price => 105000,
-      :note        => '△' * 40,
-      :details     => []}
-20.times do |t|
-  d2[:details] << {:no         => t + 1,
-                   :title      => '×××××××××',
-                   :amount     => 1,
-                   :unit_price => 5000,
-                   :price      => 5000}
+d2 = {
+  no: 3456,
+  issued_date: Time.now,
+  customer_name: 'Sample2 Co., Ltd.',
+  customer_address: '3456, Sample2cho, Sample2-shi, Shimane, Japan',
+  customer_post_code: '345-6789',
+  details: [],
+  notes: 'Estimate exsample2!'
+}
+
+d2.merge!(my_info)
+
+35.times do |t|
+  d2[:details] <<  {
+    no: t + 1,
+    name: 'xxxxxxxxxx',
+    rate: 500,
+    qty: 1,
+    amount: 500
+  }
 end
 
-estimates << d1 << d2
+data << d1 << d2
 
 # Generate reports.
 report = ThinReports::Report.create do |r|
   # Setting the layout for 'estimate.tlf'
   r.use_layout 'estimate.tlf' do |config|
     # Setting the :details list.
-    config.list do
-      use_stores :price       => 0,
-                 :total_price => 0
-      
+    config.list(:details) do
+      use_stores sub_total: 0, total: 0
       # Dispatch at list-page-footer insertion.
       events.on :page_footer_insert do |e|
-        # Set subtotal price.
-        e.section.item(:price).value(e.store.price)
-        # Initialize subtotal price to 0.
-        e.store.price = 0
+        # Set subtotal.
+        e.section.item(:sub_total).value(e.store.sub_total)
+        # Initialize subtotal to 0.
+        e.store.sub_total = 0
       end
-      
+
       # Dispatch at list-footer insertion.
       events.on :footer_insert do |e|
-        # Set total price.
-        e.section.item(:price).value(e.store.total_price)
+        # Set total.
+        e.section.item(:total).value(e.store.total)
       end
     end
   end
-  
-  estimates.each do |estimate|
+
+  data.each do |header|
     r.start_new_page
-    
-    # Set estimate datas.
-    r.page.values(:no          => estimate[:no],
-                  :created_d   => estimate[:created_d],
-                  :customer    => estimate[:customer],
-                  :title       => estimate[:title],
-                  :price       => estimate[:price],
-                  :tax         => estimate[:tax],
-                  :total_price => estimate[:total_price],
-                  :note        => estimate[:note])
-    
-    estimate[:details].each do |detail|
+
+    # Set header datas.
+    r.page.values(no: header[:no],
+      issued_date: header[:issued_date],
+      customer_name: header[:customer_name],
+      customer_address: header[:customer_address],
+      customer_post_code: header[:customer_post_code],
+      my_name: header[:my_name],
+      my_address: header[:my_address],
+      my_post_code: header[:my_post_code],
+      my_tel_number: header[:my_tel_number],
+      my_fax_number: header[:my_fax_number],
+      notes: header[:notes])
+
+    header[:details].each do |detail|
       # Add an row of list.
-      r.page.list.add_row(detail)
-      
-      # Calculate totla price.
-      r.page.list do |list|
-        list.store.price       += detail[:price]
-        list.store.total_price += detail[:price]
+      r.page.list(:details).add_row(detail)
+
+      # Calculate the amount.
+      r.page.list(:details) do |list|
+        list.store.sub_total += detail[:amount]
+        list.store.total += detail[:amount]
       end
     end
   end
 end
 
-report.generate_file('estimate.pdf')
+report.generate filename: 'estimate.pdf'
