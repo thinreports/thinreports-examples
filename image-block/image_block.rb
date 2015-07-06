1dbe2a4586efa2068a06d82047d5f41e2f25374e
maeda-m <maeda-m@matsukei.co.jp>
2015-07-06 11:25:37 +0900

    Updated TLF by requirements

--- image-block/image_block.rb
+++ image-block/image_block.rb
@@ -1,14 +1,14 @@
 # coding: utf-8
 
 require 'bundler'
-bundler.require
+Bundler.require
 
 require 'open-uri'
 
 report = Thinreports::Report.new layout: 'image_block.tlf'
 report.start_new_page do |page|
   page.item(:local_image).src('file/rails.png')
-  page.item(:remote_image).src(open('http://rubyonrails.org/images/rails.png'))
+  page.item(:remote_image).value(open('http://rubyonrails.org/images/rails.png'))
 end
 
 report.generate filename: 'result.pdf'
