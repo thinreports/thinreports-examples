56027ec7854f0c3581120a45a9d279580093586c
maeda-m <hogehogefoozoobar@gmail.com>
2015-11-26 00:21:56 +0900

    Added example: How to use Base64 in image-block

--- image-block/image_block.rb
+++ image-block/image_block.rb
@@ -5,10 +5,18 @@ Bundler.require
 
 require 'open-uri'
 
+require 'base64'
+require 'stringio'
+
 report = Thinreports::Report.new layout: 'image_block.tlf'
 report.start_new_page do |page|
   page.item(:local_image).src('file/rails.png')
   page.item(:remote_image).value(open('http://rubyonrails.org/images/rails.png'))
+
+  red_dot = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4' +
+            '//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
+  base64_image = StringIO.new(Base64.decode64(red_dot))
+  page.item(:base64_image).value(base64_image)
 end
 
 report.generate filename: 'result.pdf'
